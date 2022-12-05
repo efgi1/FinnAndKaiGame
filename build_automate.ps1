4d3b267d8674b40c95fca50844d77576288c0c7a
Nate Harper <47036497+efgi1@users.noreply.github.com>
2022-12-05 11:35:09 -0700

    Updated CMakeLists

--- build_automate.ps1
+++ build_automate.ps1
@@ -5,6 +5,6 @@ if (Test-Path ./build)
 else {
     New-Item -ItemType Directory ./build && Set-Location ./build
 }
-conan install .. -g cmake_multi -s build_type=Release --build=missing
+conan install .. -g cmake_multi -s build_type=Release
 cmake .. -G "Visual Studio 17 2022" -A x64
 cmake --build . --config Release
